cf86d22b96772ceb2e0a78d56dce268898e6c8ae
hellhoundmipt <maxfelmosmartin@gmail.com>
2015-10-14 23:09:47 +0300

    Version before review

---
+++ hw6/auxfunc.h
@@ -0,0 +1,116 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define threadsCount 2
+#define cpuCount 2
+
+struct Main2ThrMain
+{
+  int shmId;
+  int semId;
+  int exTime
+};
+
+void Add(int semId, int value)
+{
+  struct sembuf myBuf;
+  myBuf.sem_op = value;
+  myBuf.sem_num = 0;
+  myBuf.sem_flg = 0;
+  semop(semId, &myBuf, 1);
+}
+
+void Sub(int semId, int value)
+{
+  struct sembuf myBuf;
+  myBuf.sem_op = -value;
+  myBuf.sem_num = 0;
+  myBuf.sem_flg = 0;
+  semop(semId, &myBuf, 1);
+}
+
+void *ThreadMain(void *args)
+{
+  struct Main2ThrMain transmData = *(struct Main2ThrMain*)args;
+  int *data = shmat(transmData.shmId, 0, 0);
+
+  double x, y;
+  int i, yesCounter, noCounter;
+  transmData.exTime = transmData.exTime * cpuCount;
+  long int seedval;
+  struct drand48_data *buffer = calloc(1, sizeof(struct drand48_data));
+  srand48_r(seedval, buffer);
+
+  clock_t start;
+  start = clock() / CLOCKS_PER_SEC;
+
+  while((clock() / CLOCKS_PER_SEC) - start < transmData.exTime)
+  {
+    yesCounter = 0;
+    noCounter = 0;
+    for(i = 0; i < 10000; i++)
+    {
+      drand48_r(buffer, &x);
+      drand48_r(buffer, &y);
+      x = -1.0f + 2.0f * x;
+      y = -1.0f + 2.0f * y;
+      if (x*x + y*y <= 1)
+        yesCounter++;
+      else
+        noCounter++;
+    }
+    Sub(transmData.semId, 1);
+    data[0] = data[0] + yesCounter;
+    data[1] = data[1] + noCounter;
+    Add(transmData.semId, 1);
+  }
+}
+
+void MultiThread(int calcTime)
+{
+  int yesCounter, noCounter, i;
+  struct Main2ThrMain transmData;
+  transmData.exTime = calcTime;
+  key_t key = ftok("key", 0);
+  printf("key = %d\n", key);
+  transmData.semId = semget(key, 1, IPC_CREAT | 0666);
+  printf("semId = %d\n", transmData.semId);
+  transmData.shmId = shmget(key, 2*sizeof(double), IPC_CREAT | 0777);
+  printf("shmId = %d\n", transmData.shmId);
+
+  Add(transmData.semId, 1);
+
+  int *data = shmat(transmData.shmId, 0, 0);
+  data[0] = 0;
+  data[1] = 0;
+
+  pthread_t threadIndex[threadsCount];
+  for(i = 0; i < threadsCount; i++)
+  {
+    threadIndex[i] = i;
+    pthread_create(&(threadIndex[i]), 0, ThreadMain, &transmData);
+  }
+  for(i = 0; i < threadsCount; i++)
+  {
+    pthread_join(threadIndex[i], 0);
+  }
+
+  yesCounter = data[0];
+  noCounter = data[1];
+  printf("%d\t%d\n", yesCounter, noCounter);
+  long double pi = 4 * (float)yesCounter / (float)(yesCounter + noCounter);
+  printf("Pi = %lf\n", pi);
+
+  FILE *resfile = fopen("Database.txt", "a");
+  fprintf(resfile, "%d threads pi = %lf\tyesCounter = %d\tnoCounter = %d\texpected time = %d\n\n", threadsCount, pi, yesCounter, noCounter, calcTime);
+  fclose(resfile);
+
+  semctl(transmData.semId, IPC_RMID, 0);
+  shmctl(transmData.shmId, IPC_RMID, 0);
+
+}
--- hw6/mainfunc.c
+++ hw6/mainfunc.c
@@ -5,107 +5,98 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include "auxfunc.h"
+
 #define threadsCount 2
 
-struct Main2ThrMain
+struct ParsRes
 {
-  int shmId;
-  int semId;
+  int instr;
+  int calcTime;
 };
 
-void Add(int semId, int value)
+struct ParsRes ParsArgs(int argsCount, char **args)
 {
-  struct sembuf myBuf;
-  myBuf.sem_op = value;
-  myBuf.sem_num = 0;
-  myBuf.sem_flg = 0;
-  semop(semId, &myBuf, 1);
+  struct ParsRes res;
+  switch (argsCount)
+  {
+    case 2:
+      if((strcmp(args[1], "-h") == 0)  || (strcmp(args[1], "--help") == 0))
+      {
+        res.instr = 1;
+        return res;
+      }
+      break;
+
+    case 3:
+      if((strcmp(args[1], "-ot") == 0)  || (strcmp(args[1], "--onethread") == 0))
+      {
+        res.instr = 2;
+        res.calcTime = atoi(args[2]);
+        return res;
+      }
+      if((strcmp(args[1], "-mt") == 0)  || (strcmp(args[1], "--multithread") == 0))
+      {
+        res.instr = 3;
+        res.calcTime = atoi(args[2]);
+        return res;
+      }
+      break;
+  }
+  res.instr = -1;
+  return res;
 }
 
-void Sub(int semId, int value)
+void OneThread(int exTime)
 {
-  struct sembuf myBuf;
-  myBuf.sem_op = -value;
-  myBuf.sem_num = 0;
-  myBuf.sem_flg = 0;
-  semop(semId, &myBuf, 1);
-}
-
-void *ThreadMain(void *args)
-{
-  struct Main2ThrMain transmData = *(struct Main2ThrMain*)args;
-  int *data = shmat(transmData.shmId, 0, 0);
-
   double x, y;
-  int i, yesCounter, noCounter, exTime = 40;
-  long int seedval;
-  struct drand48_data *buffer = calloc(1, sizeof(struct drand48_data));
-  srand48_r(seedval, buffer);
-
-  clock_t start;
+  int yesCounter = 0, noCounter = 0;
+  clock_t start, endTime;
   start = clock() / CLOCKS_PER_SEC;
 
   while((clock() / CLOCKS_PER_SEC) - start < exTime)
   {
-    yesCounter = 0;
-    noCounter = 0;
-    for(i = 0; i < 1000; i++)
-    {
-      drand48_r(buffer, &x);
-      drand48_r(buffer, &y);
-      x = -1.0f + 2.0f * x;
-      y = -1.0f + 2.0f * y;
-      if (x*x + y*y <= 1)
-        yesCounter++;
-      else
-        noCounter++;
-    }
-    Sub(transmData.semId, 1);
-    data[0] = data[0] + yesCounter;
-    data[1] = data[1] + noCounter;
-    Add(transmData.semId, 1);
+    x = -1.0f + 2.0f * drand48();
+    y = -1.0f + 2.0f * drand48();
+    if (x*x + y*y <= 1)
+      yesCounter++;
+    else
+      noCounter++;
   }
-}
-
-int main()
-{
-  int yesCounter, noCounter, i;
-  struct Main2ThrMain transmData;
-  key_t key = ftok("key", 0);
-  printf("key = %d\n", key);
-  transmData.semId = semget(key, 1, IPC_CREAT | 0666);
-  printf("semId = %d\n", transmData.semId);
-  transmData.shmId = shmget(key, 2*sizeof(double), IPC_CREAT | 0777);
-  printf("shmId = %d\n", transmData.shmId);
-
-  Add(transmData.semId, 1);
-
-  int *data = shmat(transmData.shmId, 0, 0);
-  data[0] = 0;
-  data[1] = 0;
-
-  pthread_t threadIndex[threadsCount];
-  for(i = 0; i < threadsCount; i++)
-  {
-    threadIndex[i] = i;
-    pthread_create(&(threadIndex[i]), 0, ThreadMain, &transmData);
-  }
-  for(i = 0; i < threadsCount; i++)
-  {
-    pthread_join(threadIndex[i], 0);
-  }
-  yesCounter = data[0];
-  noCounter = data[1];
   printf("%d\t%d\n", yesCounter, noCounter);
   double pi = 4 * (float)yesCounter / (float)(yesCounter + noCounter);
   printf("Pi = %f\n", pi);
-
   FILE *resfile = fopen("Database.txt", "a");
-  fprintf(resfile, "%d threads pi = %f\tyesCounter = %d\tnoCounter = %d\n\n", threadsCount, pi, yesCounter, noCounter);
+  fprintf(resfile, "1 thread pi = %f\tyesCounter = %d\tnoCounter = %d\texpected time = %d\n\n", pi, yesCounter, noCounter, exTime);
   fclose(resfile);
+}
 
-  semctl(transmData.semId, IPC_RMID, 0);
-  shmctl(transmData.shmId, IPC_RMID, 0);
 
+int main(int argsCount, char **args)
+{
+  struct ParsRes res = ParsArgs(argsCount, args);
+  switch (res.instr)
+  {
+    case -1:
+      printf("Wrong arguments\n");
+      break;
+
+    case 1:
+      printf("HELP\n-h\t--help\t\t\t\t\tShow help\n");
+      printf("-ot\t--onethread\t[prefered time]\t\tLaunch calculation in 1 thread\n");
+      printf("-mt\t--multithread\t[prefered time]\t\tLaunch calculation in %d threads\n", threadsCount);
+      break;
+
+    case 2:
+      printf("Launching calculation of Pi in one thread\n");
+      OneThread(res.calcTime);
+      break;
+
+    case 3:
+      printf("Launching calculation of Pi in %d threads\n", threadsCount);
+      MultiThread(res.calcTime);
+      break;
+  }
   return 0;
 }
