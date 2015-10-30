1041595d95bf908a3be06ca869af25e7266c279f
hellhoundmipt <maxfelmosmartin@gmail.com>
2015-10-30 15:46:12 +0300

    First major problems

--- project/auxfile.h
+++ project/auxfile.h
@@ -1,21 +1,73 @@
-//Messages-------------------------------------------------------------------------------------------------------------------------------------------------------
+#include <stdio.h>
+#include <stdbool.h>
 
+#define maxIncidentLocs 5
+#define locsCount 8
 
-//Set locations--------------------------------------------------------------------------------------------------------------------------------------------------
-struct Location locations[10];
+/*
+Some details:
+  hero:
+  flag[0] - the hero knows about the house in the forest
+  flag[1] - the hero knows about the seceret enterace at the shore
+*/
 
-locations[0].name = (char)calloc(5, sizeof(char));
-locations[0].name = "Base";
-locations[0].incidentLocsCount = 4;
-locations[0].incidentLocs[0] = 1;
-locations[0].incidentLocs[1] = 2;
-locations[0].incidentLocs[2] = 3;
-locations[0].incidentLocs[3] = 4;
+/*Messages------------------------------------------------------------------------------------------------------------------------------------------------------*/
+void ShowDestinations(struct Loctation *locations, int currLocation)
+{
+  int i = 0;
+  for(i; i < locations[currLocation].incidentLocsCount; i++)
+    printf("%d\t%s\n", i, locations[locations[currLocation].incidentLocs[i]].name);
+}
 
-locations[0].name = (char)calloc(6, sizeof(char));
-locations[0].name = "Base";
-locations[0].incidentLocsCount = 4;
-locations[0].incidentLocs[0] = 1;
-locations[0].incidentLocs[1] = 2;
-locations[0].incidentLocs[2] = 3;
-locations[0].incidentLocs[3] = 4;
+/*Initialize locations------------------------------------------------------------------------------------------------------------------------------------------*/
+struct Location *InitLocations(struct Location *locations)
+{
+
+  locations[0].name = (char*)calloc(5, sizeof(char));
+  locations[0].name = "Base";
+  locations[0].incidentLocsCount = 3;
+  locations[0].incidentLocs[0] = 1;
+  locations[0].incidentLocs[1] = 2;
+  locations[0].incidentLocs[2] = 3;
+
+  locations[1].name = (char*)calloc(6, sizeof(char));
+  locations[1].name = "Lawn";
+  locations[1].incidentLocsCount = 1;
+  locations[1].incidentLocs[0] = 0;
+
+  locations[2].name = (char*)calloc(10, sizeof(char));
+  locations[2].name = "Enterance";
+  locations[2].incidentLocsCount = 1;
+  locations[2].incidentLocs[0] = 0;
+
+  locations[3].name = (char*)calloc(6, sizeof(char));
+  locations[3].name = "Shore";
+  locations[3].incidentLocsCount = 1;
+  locations[3].incidentLocs[0] = 0;
+
+  locations[4].name = (char*)calloc(9, sizeof(char));
+  locations[4].name = "Corridor";
+  locations[4].incidentLocsCount = 3;
+  locations[4].incidentLocs[0] = 5;
+  locations[4].incidentLocs[1] = 6;
+  locations[4].incidentLocs[2] = 7;
+
+  locations[5].name = (char*)calloc(14, sizeof(char));
+  locations[5].name = "Security room";
+  locations[5].incidentLocsCount = 1;
+  locations[5].incidentLocs[0] = 4;
+
+  locations[6].name = (char*)calloc(10, sizeof(char));
+  locations[6].name = "Canteen";
+  locations[6].incidentLocsCount = 1;
+  locations[6].incidentLocs[0] = 4;
+
+  locations[7].name = (char*)calloc(12, sizeof(char));
+  locations[7].name = "Engine room";
+  locations[7].incidentLocsCount = 0;
+
+  locations[8].name = (char*)calloc(20, sizeof(char));
+  locations[8].name = "House in the forest";
+  locations[8].incidentLocsCount = 1;
+  locations[8].incidentLocs[0] = 0;
+}
--- project/pilotquest.c
+++ project/pilotquest.c
@@ -1,8 +1,7 @@
 #include <stdbool.h>
-#include <auxfile.h>
+#include "auxfile.h"
 #include <stdio.h>
 
-const int maxIncidentLocs = 5;
 
 struct Location
 {
@@ -11,30 +10,61 @@ struct Location
   int incidentLocsCount;
 };
 
-void GameLoop()
-{
-  while(1)
-  {
-    switch(currLocation)
-    {
-      case 0:
-        printf("Where would you like to go?");
-        int i = 0;
-        for (i; i < locations[0].incidentLocsCount; i++)
-        {
-          printf("%d:\t%s\n", locations[0].incidentLocs[i], locations[i].name;);
-        }
-      break;
-    }
-  }
-}
-
 struct MainHero
 {
   bool flag[2];
 };
 
+void GameLoop()
+{
+  struct Location locations[locsCount];
+  locations = InitLocations(locations);
+  int currLocation = 0;
+  struct MainHero hero;
+  hero.flag[0] = false;
+  hero.flag[1] = false;
+
+  bool exitFlag = false;
+  while(exitFlag != true)
+  {
+    switch(currLocation)
+    {
+      case 0:
+        printf("You are at the base, deciding where to go to and what to do.\n");
+        ShowDestinations(locations, currLocation);
+        exit(0);
+      break;
+
+      case 1:
+      break;
+
+      case 2:
+      break;
+
+      case 3:
+      break;
+
+      case 4:
+      break;
+
+      case 5:
+      break;
+
+      case 6:
+      break;
+
+      case 7:
+      break;
+
+      case 8:
+      break;
+    }
+  }
+}
+
 int main()
 {
+  printf("Welcome, commader!\n");
   GameLoop();
+  printf("Thanks for playing!\n");
 }
