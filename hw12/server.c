8a706ec04d0ddcb0ea0caa25777be6f66d4edad1
hellhoundmipt <maxfelmosmartin@gmail.com>
2015-11-28 22:50:35 +0300

    Multiperson lonely chat

--- hw12/server.c
+++ hw12/server.c
@@ -6,13 +6,47 @@
 #include "arpa/inet.h"
 #include "auxfile.h"
 
-void *ServeClient(void *args);
+#define maxClients 10
 
 struct ClientsList
 {
   int clientSockDesc;
   char name[28];
 };
+
+
+void *ServeClient(void *args)
+{
+  struct ClientsList clients = *(struct ClientsList *)args;
+  int exitFlag = 0;
+  while(exitFlag == 0)
+  {
+    char message[messageLength];
+    read(clients.clientSockDesc, message, sizeof(message));
+    printf("%s\n", message);
+    if(strcmp(message, ":q") == 0)
+    {
+      close(clients.clientSockDesc);
+      printf("[%s] offline\n", clients.name);
+      return NULL;
+    }
+    write(clients.clientSockDesc, message, sizeof(message));
+    printf("Letter thrown!\n");
+  }
+}
+
+
+int SearchForName(char *name, struct ClientsList *clients, int clientsCount)
+{
+  int i;
+  for(i = 0; i < clientsCount; i++)
+  {
+    if(strcmp(name, clients[i].name) == 0)
+      return 1;
+  }
+  return 0;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -33,37 +67,32 @@ int main(int argc, char const *argv[])
     return 0;
   }
 
-  struct ClientsList clients;
+  struct ClientsList clients[maxClients];
+  int clientsCount = 0;
   pthread_t threadIds[10];
   while(1)
   {
-    listen(passiveSockDesc, 10);      //10 - размер очереди
-    //while(1) - для большого кол-ва клиентов
-    clients.clientSockDesc = accept(passiveSockDesc, 0, 0);
+    listen(passiveSockDesc, 10);
+    clients[clientsCount].clientSockDesc = accept(passiveSockDesc, 0, 0);
 
-    read(clients.clientSockDesc, clients.name, sizeof(clients.name));
-    printf("[%s] connected, descriptor: %d\n", clients.name, clients.clientSockDesc);
-    write(clients.clientSockDesc, acceptLetter, strlen(acceptLetter) + 1);
+    char name[28];
+    read(clients[clientsCount].clientSockDesc, name, sizeof(name));
+    if(SearchForName(name, clients, clientsCount) == 1)
+    {
+      printf("Access denied, name already in use.\n");
+      close(clients[clientsCount].clientSockDesc);
+    }
+    else
+    {
+      strcpy(clients[clientsCount].name, name);
+      printf("[%s] connected, descriptor: %d\n", clients[clientsCount].name, clients[clientsCount].clientSockDesc);
+      write(clients[clientsCount].clientSockDesc, acceptLetter, strlen(acceptLetter) + 1);
 
-    pthread_create(&threadIds[0], 0, ServeClient, &clients);
+      pthread_create(&threadIds[0], 0, ServeClient, &clients[clientsCount]);
+      clientsCount++;
+    }
   }
 
-  close(clients.clientSockDesc);
   close(passiveSockDesc);
   return 0;
-}
-
-void *ServeClient(void *args)
-{
-  struct ClientsList clients = *(struct ClientsList *)args;
-  int exitFlag = 0;
-  while(exitFlag == 0)
-  {
-    char message[messageLength];
-    read(clients.clientSockDesc, message, sizeof(message));
-    printf("%s\n", message);
-    write(clients.clientSockDesc, message, sizeof(message));
-    printf("Letter thrown!\n");
-  }
-  printf("Hi, man!\n");
 }
